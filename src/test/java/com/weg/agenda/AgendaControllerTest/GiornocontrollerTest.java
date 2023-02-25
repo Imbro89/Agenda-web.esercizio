95e63c890e95e6dc7b8bcd99eeb099d003e98db0
Imbro89 <francescoimbrogno8@gmail.com>
2023-02-25 12:20:32 +0100

    modified:

--- src/test/java/com/weg/agenda/AgendaControllerTest/GiornocontrollerTest.java
+++ src/test/java/com/weg/agenda/AgendaControllerTest/GiornocontrollerTest.java
@@ -54,7 +54,7 @@ public class GiornocontrollerTest {
     when(appuntamentoRepository.findAll()).thenReturn(appuntamenti);
 
     // Esecuzione del metodo da testare
-    String result = giornocontroller.index(model);
+    Object result = giornocontroller.index(model);
 
     // Verifica degli effetti collaterali
     verify(model).addAttribute("giorni", giorni);
@@ -74,7 +74,7 @@ public class GiornocontrollerTest {
     when(giornoRepository.findByData(data)).thenReturn(Optional.empty());
 
     // Esecuzione del metodo da testare
-    ResponseEntity<String> response = giornocontroller.creaGiorno(nuovoGiorno);
+    ResponseEntity<Object> response = giornocontroller.creaGiorno(nuovoGiorno);
 
     // Verifica degli effetti collaterali
     verify(giornoRepository).save(nuovoGiorno);
@@ -95,7 +95,7 @@ public class GiornocontrollerTest {
 
     // Esecuzione del metodo da testare
     Giorno nuovoGiorno = new Giorno(data);
-    String result = giornocontroller.creaGiorno(nuovoGiorno).getBody();
+    Object result = giornocontroller.creaGiorno(nuovoGiorno).getBody();
 
     // Verifica degli effetti collaterali
     verify(giornoRepository, never()).save(any());
@@ -110,7 +110,7 @@ public class GiornocontrollerTest {
     Integer id = 123;
 
     // Esecuzione del metodo da testare
-    ResponseEntity<String> result = giornocontroller.eliminaGiorno(id);
+    ResponseEntity<Object> result = giornocontroller.eliminaGiorno(id);
 
     // Verifica degli effetti collaterali
     verify(giornoRepository).deleteAppuntamentiByGiornoId(id);
@@ -129,94 +129,3 @@ public class GiornocontrollerTest {
 
 }
 
-/*
- * @RunWith(MockitoJUnitRunner.class)
- * public class GiornocontrollerTest {
- * 
- * @Mock // dipendenze repository
- * private GiornoRepository giornoRepository;
- * 
- * @Mock
- * private AppuntamentoRepository appuntamentoRepository;
- * 
- * 
- * @Mock // bean fittizio
- * private Model model;
- * 
- * @InjectMocks // dipendenza della classe controller
- * private Giornocontroller giornocontroller;
- * 
- * @Test
- * public void testIndex() {
- * // dati
- * List<Giorno> giorni = Arrays.asList(new Giorno(), new Giorno());
- * List<Appuntamento> appuntamenti = Arrays.asList(new Appuntamento(),new
- * Appuntamento());
- * when(appuntamentoRepository.findAll()).thenReturn(appuntamenti);
- * //AppuntamentoRepository risulta null
- * when(giornoRepository.findAll()).thenReturn(giorni);
- * 
- * //esecuzione dei dati
- * String result = giornocontroller.index(model);
- * 
- * // verifica dei dati
- * verify(model).addAttribute("giorni", giorni);
- * assertEquals("index", result);
- * }
- * 
- * @Test
- * public void testCreaGiorno() {
- * // dati
- * LocalDate data = LocalDate.now();
- * when(giornoRepository.findByData(data)).thenReturn(Optional.empty());
- * 
- * // esecuzione dei dati
- * String result = giornocontroller.creaGiorno(data, model);
- * 
- * // Verifica dei dati
- * verify(giornoRepository).save(any(Giorno.class));
- * assertEquals("redirect:/", result);
- * }
- * 
- * @Test
- * public void testCreaGiorno_GiornoEsistente() {
- * // dati
- * LocalDate data = LocalDate.now();
- * Giorno giornoEsistente = new Giorno(data);
- * when(giornoRepository.findByData(data)).thenReturn(Optional.of(
- * giornoEsistente));
- * 
- * // Esecuzione dei dati
- * String result = giornocontroller.creaGiorno(data, model);
- * 
- * // Verifica dei dati
- * verify(model).addAttribute("error", "Esiste già un giorno con questa data");
- * assertEquals("index", result);
- * }
- * 
- * @Test
- * public void testEliminaGiorno() {
- * // dati
- * int id = 1;
- * 
- * // Esecuzione dei dati
- * String result = giornocontroller.eliminaGiorno(id, model);
- * 
- * // verifica dei dati
- * verify(giornoRepository).deleteAppuntamentiByGiornoId(id);
- * verify(giornoRepository).deleteById(id);
- * assertEquals("redirect:/", result);
- * }
- * 
- * @Test(expected = IllegalArgumentException.class)
- * public void testEliminaGiorno_NullId() {
- * // dati
- * Integer id = null;
- * 
- * // Esecuzione dei dati
- * giornocontroller.eliminaGiorno(id, model);
- * 
- * }
- * 
- * }
- */
